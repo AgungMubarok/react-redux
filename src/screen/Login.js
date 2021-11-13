d5ab3365991a812d28dd3bbb3a2a485f262faf5e
Agung Mubarok <agungmubarok555@gmail.com>
2021-11-13 09:39:00 +0700

    add form

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Layout from '../layout/Layout';
 
 const Login = () => {
+  
+  const { register, handleSubmit, formState: {errors} } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  }
+  
   return (
     <Layout>
-      ini login
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <div className="col-md-4">
+          <label htmlFor="firstName" className="form-label">First name</label>
+          <input 
+            type="text" 
+            className={errors?.firstName ? "is-invalid form-control" : "form-control"}
+            {...register("firstName", {required: true},)}
+          />
+          <div className="invalid-feedback">
+            {errors.firstName && errors.firstName.type === "required" && "form harus diisi"}
+          </div>
+        </div>
+        <input type="submit" />
+      </form>
     </Layout>
   );
 };
